c8f49317a422f279a0330f095eceb25b5c3ed20f
Trevor <me@trev.app>
2024-08-13 01:18:27 -0400

    Base wizard

---
+++ MelonLoader.VSExtension/WizardImplementation.cs
@@ -0,0 +1,42 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace MelonLoader.VSExtension
+{
+    public class WizardImplementation : IWizard
+    {
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            try
+            {
+                WizardDialog inputForm = new WizardDialog();
+                inputForm.ShowDialog();
+
+                replacementsDictionary.Add("$custommessage$", inputForm.CustomMessage);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        // These methods are only called for item templates, not for project templates.
+        public bool ShouldAddProjectItem(string filePath) => true;
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem) { }
+    }
+}
---
+++ MelonLoader.VSTemplate/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MelonLoader.VSTemplate
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("$custommessage$");
+        }
+    }
+}
